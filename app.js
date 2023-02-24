0f41e6c50c79a0b1b134fd1ab01357de60480180
*SonHaiDang* <haidangson09@gmail.com>
2023-02-24 21:36:06 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const app = express()
+const app = express();
+const contactsRouter = require('./app/routes/contact.route');
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.use('/api/contacts', contactsRouter);
 
 module.exports = app;
---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,25 @@
+
+exports.create = (req,res) =>{
+    res.send('Create contact');
+}
+exports.findAll = (req,res) =>{
+    res.send('findAll contact');
+}
+exports.findOne = (req,res) =>{
+    res.send('findOne contact');
+}
+exports.update = (req,res) =>{
+    res.send('update contact');
+}
+exports.delete = (req,res) =>{
+    res.send('delete contact');
+}
+exports.deleteAll = (req,res) =>{
+    res.send('deleteAll contact');
+}
+exports.findAllFavorite = (req,res) =>{
+    res.send('findAllFavorite contact');
+}
+exports.create = (req,res) =>{
+    res.send('Create contact');
+}
