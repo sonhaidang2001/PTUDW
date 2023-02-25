e567e0b79471fb3933f1187a1a6240a235f5b802
*SonHaiDang* <haidangson09@gmail.com>
2023-02-25 10:40:56 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,25 +1,88 @@
+const ApiError = require('../api-error');
+const ContactService = require('../services/contact.service');
+const MongDB = require('../untils/mongdb.until');
 
-exports.create = (req,res) =>{
-    res.send('Create contact');
+exports.create = async (req,res,next) =>{
+    if(!req.body?. name){
+        return next(new ApiError(400,"Name can not empty"));
+    }
+    try {
+        const contactService = new ContactService(MongDB.client);
+        const document = await contactService.create(req.body);
+        return res.send(document);
+    } catch (error) {
+        return next(new ApiError(500,"An error occurred while creating the contact"));
+    };
 }
-exports.findAll = (req,res) =>{
-    res.send('findAll contact');
+exports.findAll = async (req,res,next) =>{
+   let document = [];
+   try {
+         const contactService = new ContactService(MongDB.client);
+         const{name} = req.query;
+         if(name){
+            document = await contactService.findByName(name);
+        }else{
+             document = await contactService.find({});            
+         }
+   } catch (error) {
+        return next(new ApiError(500,"An error occurred while retrieving the contact"));
+   }
 }
-exports.findOne = (req,res) =>{
-    res.send('findOne contact');
+exports.findOne = async (req,res,next) =>{
+        try {
+            const contactService = new ContactService(MongDB.client);
+            const document = await contactService.create(req.params.id);
+            if(!document){
+                return next(new ApiError(404,"Contact not found"));
+            }
+        } catch (error) {
+            return next(new ApiError(500,`Error retrieving contact with id=${req.params.id}`));
+        };
 }
-exports.update = (req,res) =>{
-    res.send('update contact');
+exports.update = async (req,res,next) =>{
+    if(Object.keys(req.body).length ===0){
+        return next(new ApiError(404,"Data update can not be empty"));
+    }
+    try {
+        const contactService = new ContactService(MongDB.client);
+        const document = await contactService.update(req.params.id,req.body);
+        if(!document){
+            return next(new ApiError(404,"Contact not found"));
+        }else{
+            return res.send({message: "Contact was update successfully"});
+        }
+    } catch (error) {
+        return next(new ApiError(500,`Error update contact with id=${req.params.id}`));
+    }
 }
-exports.delete = (req,res) =>{
-    res.send('delete contact');
+exports.delete = async (req,res,next) =>{
+    try {
+        const contactService = new ContactService(MongDB.client);
+        const document = await contactService.delete(req.params.id);
+        if(!document){
+            return next(new ApiError(404,"Contact not found"));
+        }else{
+            return res.send({message: "Contact was delete successfully"});
+        }
+    } catch (error) {
+        return next(new ApiError(500,`Could not delete contact with id=${req.params.id}`));
+    }
 }
-exports.deleteAll = (req,res) =>{
-    res.send('deleteAll contact');
+exports.deleteAll = async (req,res,next) =>{
+    try {
+        const contactService = new ContactService(MongDB.client);
+        const document = await contactService.deleteAll();
+        return res.send({message: `${document} Contact were delete successfully `});
+    } catch (error) {
+        return next(new ApiError(500,"An error occurred while removing all the contact"));
+    }
 }
-exports.findAllFavorite = (req,res) =>{
-    res.send('findAllFavorite contact');
-}
-exports.create = (req,res) =>{
-    res.send('Create contact');
+exports.findAllFavorite = async (req,res,next) =>{
+    try {
+        const contactService = new ContactService(MongDB.client);
+        const document = await contactService.findAllFavorite();
+        return res.send(document);
+    } catch (error) {
+        return next(new ApiError(500,"An error occurred while favorite the contact"));
+    }
 }
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const app = require('./app');
 const config = require('./app/config/index');
+const MongDB = require('./app/untils/mongdb.until');
 
-const PORT = config.app.port;
-app.listen(PORT, ()=>{
-    console.log(`Server running is http://localhost:${PORT}`);
-})
+async function startServer() {
+    try {
+        await MongDB.connect(config.db.uri);
+        console.log('Connected to the database');
 
+        const PORT = config.app.port;
+        app.listen(PORT, ()=>{
+                console.log(`Server running is http://localhost:${PORT}`);
+            })            
+    } catch (error) {
+        console.log('Connect can not database!!',error);
+        process.exit();
+    }
+}
 
+startServer();
