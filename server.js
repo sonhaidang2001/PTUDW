00deaa719625acc7f385939f22c5726b26f50a4c
shdangit <shdangit@gmail.com>
2023-04-07 12:37:10 +0700

    cai dat xu li loi

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const contactRouter = require('./app/routers/contact.route')
-// const ApiError = require('./app/api-error');
+const ApiError = require('./app/api-error');
 // const db = require('./database/db');
 
 // db 
@@ -18,15 +18,16 @@ app.use(express.json());
 app.use('/api/contacts',contactRouter)
 
 // error 
-// app.use((req, res, next) => {   
-//     return next(new ApiError(404, "Resource not found"));
-// });
+app.use((req, res, next) => {   
+    return next(new ApiError(404, "Resource not found"));
+});
 
-// app.use((err, req, res, next) => {  
-//     return res.status(err.statusCode || 500).json({
-//     message: err.message || "Internal Server Error",
-//     });
-// });
+app.use((err, req, res, next) => {  
+    return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+    });
+});
+
 app.use('/',(req,res)=>{
     res.send('helo');
 })
